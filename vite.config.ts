8ead8f6344129de367f15aa84e9f5d90946de3cc
Timothy Kempf <tim@kempf.dev>
2025-04-12 22:02:36 -0700

    move to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     svelte({
       compilerOptions: {
